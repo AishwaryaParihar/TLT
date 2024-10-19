e4346462c5334e0f53107a2e77a597f31a526231
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-10-19 18:01:18 +0530

    this chnage for deploy project on digital

--- Student-Panel/src/Common/Summary.js
+++ Student-Panel/src/Common/Summary.js
@@ -1,4 +1,4 @@
-const backendDomain = 'http://localhost:8080';
+const backendDomain = 'https://sea-lion-app-uytpg.ondigitalocean.app';
 // const backendDomain = "https://tlt-project-6ivu.onrender.com";
 
 const SummaryApi = {
